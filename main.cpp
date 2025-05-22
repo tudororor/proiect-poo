f9dee524c2addeb5baba6609dbb3d4e15e1284b0
tudororor <tudor.205.205@gmail.com>
2025-05-22 05:39:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,32 +10,6 @@
 #include "abonamentPremium.h"
 
 int main() {
-    // Sala sala("Sala Mare", 70, 60, {
-    //     {"Luni", {10, 11}}
-    // });
-    //
-    // Rezervare<Sala> r(sala, "Luni", 10, "Gaozmin");
-    //
-    // r.afiseaza();
-    //
-    // Client c("X", "Y", "a@a.com", "123", 22);
-    // c.afiseazaProfil();
-    //
-    // Instructor i("X", "Y", "a@a.com", "123", 22);
-    // i.afiseazaProfil();
-    //
-    // Administrator a("X", "Y", "a@a.com", "123", 22);
-    // a.afiseazaProfil();
-    //
-    // std::cout << "Numele salii: " << sala.getNume() << '\n';
-    //
-    // if (sala.esteLiberaLa("Luni", 10))
-    //     std::cout << "Sala este libera Luni la 10\n";
-    // else
-    //     std::cout << "Sala este ocupata Luni la 10\n";
-    //
-    // sala.ocupaOra("Luni", 11);
-
     AbonamentStandard abs;
     abs.afiseazaDetalii();
 
@@ -43,13 +17,12 @@ int main() {
     abp.afiseazaDetalii();
 
     std::string sala = "Sala Mare";
-
     for (int i = 0; i < 3; ++i) {
         if (abp.poateRezervaSala(sala)) {
-            std::cout << "Rezervare #" << i + 1 << " permisa\n";
+            std::cout << "Rezervare #" << i + 1 << " permisă\n";
             abp.inregistreazaRezervare(sala);
         } else {
-            std::cout << "Rezervare #" << i + 1 << " REFUZATA pentru " << sala << "\n";
+            std::cout << "Rezervare #" << i + 1 << " REFUZATĂ pentru " << sala << "\n";
         }
     }
 
@@ -57,21 +30,18 @@ int main() {
     apr.afiseazaDetalii();
 
     std::string sala2 = "Sala Mica";
-
     for (int i = 1; i <= 5; ++i) {
         if (apr.poateRezervaSala(sala2)) {
-            std::cout << "Rezervare #" << i << " permisa\n";
+            std::cout << "Rezervare #" << i << " permisă\n";
             apr.inregistreazaRezervare(sala2);
         } else {
-            std::cout << "Rezervare #" << i << " REFUZATA pentru " << sala2 << "\n";
+            std::cout << "Rezervare #" << i << " REFUZATĂ pentru " << sala2 << "\n";
         }
     }
 
     std::cout << "Discount la evenimente: " << apr.getDiscountEvenimente() << "%\n";
 
-
-    std::cout << "\nApasa Enter pentru a iesi...";
+    std::cout << "\nApasă Enter pentru a ieși...";
     std::cin.get();
     return 0;
-
 }
